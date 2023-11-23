2ab80dabcfe76b0fd2479e0f36e4250a0c0de829
ChenMachluf101 <146337999+ChenMachluf101@users.noreply.github.com>
2023-11-23 10:51:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 // @ts-ignore
 import gsap from "gsap";
-import {Application, Container} from "pixi.js";
+import { Application, Container } from "pixi.js";
 // @ts-ignore
-import {MyCircle} from "./MyCircle.js";
+import { MyCircle } from "./MyCircle.js";
 // @ts-ignore
-import {MyRectangle} from "./MyRectangle.js";
+import { MyRectangle } from "./MyRectangle.js";
 // @ts-ignore
-import {MyArc} from "./MyArc.js";
+import { MyArc } from "./MyArc.js";
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x58E02C,
-	width:1200,
-	height: 600
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x58e02c,
+  width: 1200,
+  height: 600,
 });
 
 const smallRightCirCont = new Container();
@@ -24,69 +24,226 @@ const bigLeftCircCont = new Container();
 const rectanglesCont = new Container();
 const bigCircleRadius = 150;
 const smallCircleRadius = 30;
-const rectangleRadius = 0 ;
+const rectangleRadius = 0;
 const screenW = app.screen.width;
 const screenH = app.screen.height;
 const smallWhiteCircleX = screenW / 4 + bigCircleRadius - smallCircleRadius;
-const smallBlackCircleX = screenW * 3 / 4 - bigCircleRadius + smallCircleRadius;
+const smallBlackCircleX =
+  (screenW * 3) / 4 - bigCircleRadius + smallCircleRadius;
 const black = 0x000000;
-const white = 0xFFFFFF;
+const white = 0xffffff;
 
+const whiteRectangleConfig = {
+  x: -screenW * 2,
+  y: -screenH,
+  width: screenW * 3,
+  height: screenH * 3,
+  radius: rectangleRadius,
+  color: white,
+  container: rectanglesCont,
+};
+new MyRectangle(whiteRectangleConfig);
 
-new MyRectangle(- screenW * 2,  - screenH, screenW * 3 , screenH * 3, rectangleRadius, white, rectanglesCont);
-new MyRectangle(screenW / 2 , - screenH, screenW  * 3, screenH * 3, rectangleRadius, black, rectanglesCont);
-const topLeftArc = new MyArc(screenW / 4,screenH / 2, bigCircleRadius,Math.PI, 2 * Math.PI,black, bigLeftCircCont);
-const bottomLeftArc = new MyArc(screenW / 4, screenH / 2, bigCircleRadius, Math.PI, 2 * Math.PI,black, bigLeftCircCont,false,false,true);
-const smallLeftCircle = new MyCircle(smallWhiteCircleX, screenH / 2, smallCircleRadius, white, smallLeftCircCont,screenW / 4, screenH / 2);
-const topRightArc = new MyArc(screenW * 3 / 4,screenH / 2,bigCircleRadius,Math.PI, 2 * Math.PI,white, bigRightCircCont,screenW * 3 / 4 , screenH / 2);
-const bottomRightArc = new MyArc(screenW * 3 / 4, screenH / 2, bigCircleRadius, Math.PI, 2 * Math.PI,white, bigRightCircCont,screenW * 3 / 4 , screenH / 2,true);
-const smallRightCircle = new MyCircle(smallBlackCircleX, screenH / 2, smallCircleRadius, black, smallRightCirCont, screenW * 3 / 4 , screenH / 2);
+const blackRectangleConfig = {
+  x: screenW / 2,
+  y: -screenH,
+  width: screenW * 3,
+  height: screenH * 3,
+  radius: rectangleRadius,
+  color: black,
+  container: rectanglesCont,
+};
+
+new MyRectangle(blackRectangleConfig);
+
+const topLeftArcConfig = {
+  x: screenW / 4,
+  y: screenH / 2,
+  radius: bigCircleRadius,
+  startAngle: Math.PI,
+  endAngle: 2 * Math.PI,
+  color: black,
+  container: bigLeftCircCont,
+};
+
+const topLeftArc = new MyArc(topLeftArcConfig);
+
+const bottomLeftArcConfig = {
+  x: screenW / 4,
+  y: screenH / 2,
+  radius: bigCircleRadius,
+  startAngle: Math.PI,
+  endAngle: 2 * Math.PI,
+  color: black,
+  container: bigLeftCircCont,
+  pivotX: false,
+  pivotY: false,
+  anticlockwise: true,
+};
+const bottomLeftArc = new MyArc(bottomLeftArcConfig);
+
+const smallLeftCircleConfig = {
+  x: smallWhiteCircleX,
+  y: screenH / 2,
+  radius: smallCircleRadius,
+  color: white,
+  container: smallLeftCircCont,
+  pivotX: screenW / 4,
+  pivotY: screenH / 2,
+};
+
+const smallLeftCircle = new MyCircle(smallLeftCircleConfig);
+
+const topRightArcConfig = {
+  x: (screenW * 3) / 4,
+  y: screenH / 2,
+  radius: bigCircleRadius,
+  startAngle: Math.PI,
+  endAngle: 2 * Math.PI,
+  color: white,
+  container: bigRightCircCont,
+  pivotX: (screenW * 3) / 4,
+  pivotY: screenH / 2,
+};
+
+const topRightArc = new MyArc(topRightArcConfig);
+
+const bottomRightArcConfig = {
+  x: (screenW * 3) / 4,
+  y: screenH / 2,
+  radius: bigCircleRadius,
+  startAngle: Math.PI,
+  endAngle: 2 * Math.PI,
+  color: white,
+  container: bigRightCircCont,
+  pivotX: (screenW * 3) / 4,
+  pivotY: screenH / 2,
+  anticlockwise: true,
+};
+
+const bottomRightArc = new MyArc(bottomRightArcConfig);
+
+const smallRightCircleConfig = {
+  x: smallBlackCircleX,
+  y: screenH / 2,
+  radius: smallCircleRadius,
+  color: black,
+  container: smallRightCirCont,
+  pivotX: (screenW * 3) / 4,
+  pivotY: screenH / 2,
+};
+
+const smallRightCircle = new MyCircle(smallRightCircleConfig);
 
 bigRightCircCont.addChild(smallRightCirCont);
 bigLeftCircCont.addChild(smallLeftCircCont);
-app.stage.addChild(rectanglesCont, bigLeftCircCont,bigRightCircCont);
+app.stage.addChild(rectanglesCont, bigLeftCircCont, bigRightCircCont);
 play();
 
-function play(){
-	playCircles();
-	rotateRectangle(1.05,1.57,2.1,3.14, black, white,3000);
-	rotateRectangle(4.19,4.71,5.24,6.2848, white, black,7000);
-	rotateRectangle(7.33,7.854,8.374,9.425, black, white,10000);
-	rotateRectangle(10.475,10.995,11.525,12.565, white, black,13000);
+// @ts-ignore
+function play() {
+  playCircles();
+  rotateRectangle(1.05, 1.57, 2.1, 3.14, black, white);
+  // rectanglesCont.rotation = 0;
+  // rotateRectangle(1.05, 1.57, 2.1, 3.14, white, black);
+  // rotateRectangle(4.19, 4.71, 5.24, 6.2848, white, black);
+  // rotateRectangle(7.33, 7.854, 8.374, 9.425, black, white);
+  // rotateRectangle(10.475, 10.995, 11.525, 12.565, white, black);
 }
 
-
-
-function playCircles(){
-	gsap.from (bigRightCircCont, { rotation: 0, y: - 300, duration: 1,repeat: -1, repeatDelay: 3, startAt: {x: screenW * 3 / 4,y: 300, opacity: 0} });
-	gsap.from (bigRightCircCont, { rotation: 0, y: - 300, duration: 1,repeat: -1, repeatDelay: 10, startAt: {x: screenW * 3 / 4,y: 300, opacity: 0} });
-	gsap.to (smallLeftCircCont, {  duration: 3,rotation: -6.28 , repeat: -1});
-	gsap.to (smallRightCirCont, { duration: 2,rotation: -6.28, repeat: -1 });
+// @ts-ignore
+function playCircles() {
+  gsap.from(bigRightCircCont, {
+    rotation: 0,
+    y: -300,
+    duration: 1,
+    repeat: -1,
+    repeatDelay: 3,
+    startAt: { x: (screenW * 3) / 4, y: 300, opacity: 0 },
+  });
+  gsap.from(bigRightCircCont, {
+    rotation: 0,
+    y: -300,
+    duration: 1,
+    repeat: -1,
+    repeatDelay: 10,
+    startAt: { x: (screenW * 3) / 4, y: 300, opacity: 0 },
+  });
+  gsap.to(smallLeftCircCont, { duration: 3, rotation: -6.28, repeat: -1 });
+  gsap.to(smallRightCirCont, { duration: 2, rotation: -6.28, repeat: -1 });
 }
-function rotateRectangle(rotation1: number, rotation2: number, rotation3: number, rotation4: number, leftColor: number, rightColor: number, timeout: number){
-	setTimeout(() => {firstStep(rotation1)},timeout);
-	setTimeout(() => {secondStep(rotation2, leftColor, rightColor)},timeout + 50);
-	setTimeout(() => {thirdStep(rotation3, leftColor, rightColor)},timeout + 100);
-	setTimeout(() => {lastStep(rotation4)},timeout + 150);
+
+function rotateRectangle(
+  rotation1: number,
+  rotation2: number,
+  rotation3: number,
+  rotation4: number,
+  leftColor: number,
+  rightColor: number,
+) {
+  firstStep(rotation1)
+    .then(() => {
+      console.log("after first step");
+      return secondStep(rotation2, leftColor, rightColor);
+    })
+    .then(() => {
+      return thirdStep(rotation3, leftColor, rightColor);
+    })
+    .then(() => {
+      return lastStep(rotation4);
+    })
+    .then(() => {
+      console.log("finished");
+    });
 }
+
+// @ts-ignore
 function firstStep(rotation: number) {
-	gsap.to(rectanglesCont, {duration: 0.2,ease: "none",delay: 2000,  rotation:rotation});
+  return gsap
+    .to(rectanglesCont, {
+      duration: 0.2,
+      ease: "none",
+      delay: 2000,
+      rotation: rotation,
+    })
+    .then(() => {});
 }
-
+// @ts-ignore
 function secondStep(rotation: number, colorLeft: number, colorRight: number) {
-	gsap.to(rectanglesCont, { duration: 0.15,ease: "none",  rotation:rotation});
-	setTimeout(() => {topRightArc.changeColor(colorLeft)}, 100);
-	setTimeout(() => {bottomLeftArc.changeColor(colorRight)}, 100);
+  console.log("in second step");
+  return gsap
+    .to(rectanglesCont, { duration: 0.15, ease: "none", rotation: rotation })
+    .then(() => {
+      topRightArc.changeColor(colorLeft);
+      bottomLeftArc.changeColor(colorRight);
+    });
 }
 
+// @ts-ignore
 function thirdStep(rotation: number, colorLeft: number, colorRight: number) {
-	gsap.to(rectanglesCont, { duration: 0.15,ease: "none",  rotation:rotation});
-	setTimeout(() => {topLeftArc.changeColor(colorRight)}, 100);
-	setTimeout(() => {smallLeftCircle.changeColor(colorLeft)}, 100);
-	setTimeout(() => {bottomRightArc.changeColor(colorLeft)}, 100);
-	setTimeout(() => {smallRightCircle.changeColor(colorRight)}, 100);
+  console.log("in third step");
+  return gsap
+    .to(rectanglesCont, {
+      duration: 0.15,
+      ease: "none",
+      rotation: rotation,
+    })
+    .then(() => {
+      topLeftArc.changeColor(colorRight);
+      smallLeftCircle.changeColor(colorLeft);
+      bottomRightArc.changeColor(colorLeft);
+      smallRightCircle.changeColor(colorRight);
+    });
 }
 
+// @ts-ignore
 function lastStep(rotation: number) {
-	gsap.to(rectanglesCont, {duration: 0.2,ease: "power2.out", rotation: rotation});
+  console.log("in last step");
+  return gsap
+    .to(rectanglesCont, {
+      duration: 0.2,
+      ease: "power2.out",
+      rotation: rotation,
+    })
+    .then(() => {});
 }
